e1ae951b536e4c629ed5fadfa8994f1839534b4e
shm_usuf <shmusuf@gmail.com>
2023-06-01 17:05:25 +0600

    category com & categoryImg initialized

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,5 +1,15 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
 const Category = () => {
-  return <section className="py-20">Category</section>;
+  return (
+    <section className="py-20">
+      {/* SECTION GOES HERE */}
+      <div className="w-full h-[50vh] bg-yellow-400"></div>
+    </section>
+  );
 };
 
 export default Category;
